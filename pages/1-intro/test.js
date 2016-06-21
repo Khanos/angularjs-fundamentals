bb2034e285635e197c394392d37afd3d55e8bdd9
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2016-06-21 10:26:32 -0400

    Test

---
+++ pages/1-intro/test.js
@@ -0,0 +1,32 @@
+var a='badsdjasd';
+var b=12127182;
+var arr=[];
+arr=[1,2,3,4];
+var arrm=[1,'sjdkjsd','sdlhdlsdh'];
+var objtest={
+    name: 'skdksjd',
+    age: 8787,
+    arr:[],
+    run: function() {
+        return true;
+    }
+};
+objtest.test='skjdksjd';
+
+function test() {
+    return true;
+}
+
+var test = function() {
+
+}
+
+test();
+
+
+
+(function() {
+    'use strict';
+
+
+})();
